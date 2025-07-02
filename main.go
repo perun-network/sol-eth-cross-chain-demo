af48d6e5d571b6ea499bc9fdb52079d2e9e325cf
Minh Huy Tran <huy@perun.network>
2025-07-02 10:26:18 +0200

    feat: add assets
    Signed-off-by: Minh Huy Tran <huy@perun.network>

--- main.go
+++ main.go
@@ -32,6 +32,8 @@ const (
 	keyDeployer = "79ea8f62d97bc0591a4224c1725fca6b00de5b2cea286fe2e0bb35c5e76be46e"
 	keyAlice    = "1af2e950272dd403de7a5760d41c6e44d92b6d02797e51810795ff03cc2cda4f"
 	keyBob      = "f63d7d8e930bccd74e93cf5662fde2c28fd8be95edb70c73f1bdd863d07f412e"
+
+	PerunAddress = "GQtQCW4dREybk2FR1gabaSb89CFxGrNS74JX5fZ97Qmh"
 )
 
 func main() {
